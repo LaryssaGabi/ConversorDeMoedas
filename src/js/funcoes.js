a81c009393721db7ac804cd4af5b39c43ac160f5
Laryssa Gabriela <111398446+LaryssaGabi@users.noreply.github.com>
2024-04-14 19:09:19 -0300

    Update funcoes.js

--- src/js/funcoes.js
+++ src/js/funcoes.js
@@ -1,22 +1,22 @@
 function converter() {
-    var valorConverter = Number(document.getElementById("valor").value);
-    var moedaPara = document.querySelector('.moeda-para').value;
-    var moedaDe = document.querySelector('.moeda-de').value;
-    var taxaDeCambio = {
+    let valorConverter = Number(document.getElementById("valor").value);
+    let moedaPara = document.querySelector('.moeda-para').value;
+    let moedaDe = document.querySelector('.moeda-de').value;
+    let taxaDeCambio = {
         'BRL': 1,
         'USD': 4.89, //1 real é igual a 0,20.
         'EUR': 5.37,//1 real é igual a 0,19.
         'GBP': 6.24 //1 real é igual a 0,16.
     };
 
-    var nomesMoedas = {
+    let nomesMoedas = {
         'BRL': 'Real Brasileiro',
         'USD': 'Dólar Americano',
         'EUR': 'Euro',
         'GBP': 'Libra Esterlina'
     };
 
-    var valorConvertido = 0;
+    let valorConvertido = 0;
 
     // Verifica se o valor é válido
     if (isNaN(valorConverter) || valorConverter <= 0) {
@@ -38,8 +38,8 @@ function converter() {
 // Função para atualizar os resultados na página
 function atualizarResultados(valorConverter, valorConvertido, moedaDe, moedaPara, nomesMoedas) {
     // Formatar os valores numéricos
-    var formatoMoedaDe = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: moedaDe });
-    var formatoMoedaPara = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: moedaPara });
+    let formatoMoedaDe = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: moedaDe });
+    let formatoMoedaPara = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: moedaPara });
 
     // Atualiza os valores na página HTML
     document.querySelector('.resultado-item-de .logo').src = `./src/img/${moedaDe.toLowerCase()}.png`;
